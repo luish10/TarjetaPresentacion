a80a9ab137b365aadd475fc1e5467dc2fe91316f
Luis Hernandez <luis.hdz164@gmail.com>
2019-11-01 21:07:32 -0600

    Se cambiaron los fondos

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-function test() {
-    alert("Funcion test ejecutada");
-
-   let input = document.getElementById("input");
-   console.log(input.value);
-    let prueba = document.getElementById("prueba");
-    console.log(prueba.innerHTML);
-    prueba.innerHTML = input.value; 
-    prueba.classList.add("is-red");
-}
+/*** rebuilt http://dribbble.com/shots/790267-Mini-Profile-PS with CSS **/
