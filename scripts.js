bff3f45834ebfe9c1d8241e01acf3b2d9b0ebd53
Luis Hernandez <luis.hdz164@gmail.com>
2019-11-01 20:31:20 -0600

    Se agrego un encabezado y un parrafo

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function test() {
+    alert("Funcion test ejecutada");
+
+   let input = document.getElementById("input");
+   console.log(input.value);
+    let prueba = document.getElementById("prueba");
+    console.log(prueba.innerHTML);
+    prueba.innerHTML = input.value;
+    prueba.classList.add("is-red");
+}
